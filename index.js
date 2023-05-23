9f444f2597dc34688dcf2e53a4671a6ab0bd13cf
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-23 13:01:47 -0500

    adding credentials

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from "express";
 import router from "./lib/routes/indexRouter.js";
 import * as dotenv from 'dotenv';
-/* import cors from 'cors' */
 
 const app = express();
 dotenv.config()
 app.use(express.json());
 
-/* const corsOptions ={
-  origin:'10.0.2.2', //change to deployed url
-  credentials:true,
-  optionSuccessStatus:200,
-}
-app.use(cors(corsOptions)); */
 
 app.use('/', router);
 
