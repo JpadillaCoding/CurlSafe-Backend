295118dd8da921f204b0e8c1cf24ab8a85f1b331
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-22 21:29:40 -0500

    feat: setting env for heroku

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import router from "./lib/routes/indexRouter.js";
-import cors from 'cors'
+import * as dotenv from 'dotenv';
+/* import cors from 'cors' */
 
 const app = express();
+dotenv.config()
 app.use(express.json());
-const corsOptions ={
+
+/* const corsOptions ={
   origin:'10.0.2.2', //change to deployed url
   credentials:true,
   optionSuccessStatus:200,
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); */
 
 app.use('/', router);
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("Clean up on isle 4000");
 });
